27086d50e852d3ddc6b60295fcadcf3f6545b5dd
Salika155 <62901117+Salika155@users.noreply.github.com>
2022-11-15 17:46:51 +0100

    Add files via upload

---
+++ ex_program_eva/ex_program_eva/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex_program_eva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

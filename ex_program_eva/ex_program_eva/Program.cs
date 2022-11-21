eebc78d25bae8f1a59e9601d88eae35e2b078745
Salika155 <carre155@hotmail.com>
2022-11-21 01:04:53 +0100

    a
    cambios ejercicio

--- ex_program_eva/ex_program_eva/Program.cs
+++ ex_program_eva/ex_program_eva/Program.cs
@@ -11,9 +11,12 @@ namespace ex_program_eva
 
         }
 
-
         //Escribir una función que calcule el área de un círculo y otra que calcule el volumen de un cilindro usando la primera función.
 
+        public static double GetAreaCirc(double radius)
+        {
+            return (radius * radius) * Math.PI;
+        }
 
 
 

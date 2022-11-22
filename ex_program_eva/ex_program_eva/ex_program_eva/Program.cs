f1b98bb0ce7d00cb99f11266fa845f60545aa06f
Salika155 <62901117+Salika155@users.noreply.github.com>
2022-11-22 17:55:05 +0100

    z
    a

---
+++ ex_program_eva/ex_program_eva/ex_program_eva/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+
+namespace ex_program_eva
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("El área del círculo es = ");
+            Console.WriteLine(Exercises_ex.GetAreaCirc(1));
+            Console.WriteLine("El volumen del cilindro es = ");
+            Console.WriteLine(Exercises_ex.GetCylindreVolume(2, 4));
+        }
+
+    }
+}        

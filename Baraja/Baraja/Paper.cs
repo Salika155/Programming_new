0608bc00dc162d78ba7302b99431f01f4f3f7856
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-01-27 02:03:44 +0100

    Add files via upload

---
+++ Baraja/Baraja/Paper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Baraja;
+
+namespace Baraja
+{
+
+    public enum CardType
+    {
+        Oros,
+        Espadas,
+        Bastos,
+        Copas
+    }
+        //enum con palos
+    class Carta
+    {
+        //Atributos privados
+        private int _numero;
+        private CardType _type; 
+
+        //Properties
+        public int Numero => _numero; 
+        public CardType Type => _type; 
+
+        //Constructores
+        public Carta(CardType type)
+        {
+            
+            CardType carta = CardType.Oros;
+        }
+
+
+        public Carta(int numero, CardType palo)
+        {
+            this._numero = Numero;
+            this._type = Type;
+        }
+    }
+}
+    

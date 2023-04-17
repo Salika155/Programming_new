cd02dd317cf450529d556468704d0d438be140e2
Javier Marín <56446732+jmarinprofe@users.noreply.github.com>
2023-04-17 09:45:04 +0200

    Update Board3.cs

--- EV3/BuscaminasBiblio/BuscaminasBiblio/Board3.cs
+++ EV3/BuscaminasBiblio/BuscaminasBiblio/Board3.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace BuscaminasBiblio
 {
+    // Javi: Clase sin hacer
     public class Board3 : IBoard
     {
         void IBoard.CreateBoard(int width, int height)

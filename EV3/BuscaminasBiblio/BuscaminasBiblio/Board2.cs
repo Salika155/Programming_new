070f9c2cc1811c88b567265ba85134b3fbb5ff9d
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-04-18 00:09:06 +0200

    Update Board2.cs

--- EV3/BuscaminasBiblio/BuscaminasBiblio/Board2.cs
+++ EV3/BuscaminasBiblio/BuscaminasBiblio/Board2.cs
@@ -7,66 +7,126 @@ using System.Threading.Tasks;
 namespace BuscaminasBiblio
 {
     // Javi: Clase sin hacer
-    public class Board2 : IBoard
+     public class Board2 : IBoard
     {
-        void IBoard.CreateBoard(int width, int height)
+        Bombs[] bombs = new Bombs[10];
+        Cells[] cells = new Cells[100];
+        Flags[] flags = new Flags[100];
+
+        private int _width;
+        private int _height;
+
+        public bool IsOpen(int x, int y)
         {
             throw new NotImplementedException();
         }
 
-        void IBoard.DeleteFlagAt(int x, int y)
+        public void CreateBoard(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            throw new Exception("No puedes crear un tablero de ese tamaño");
+        }
+
+        public void DeleteFlagAt(int x, int y)
+        {
+            for (int i = 0; i < flags.Length; i++)
+                if (flags[i].position.x == x && flags[i].position.y == y);
+                    
+        }
+
+        public int GetBombProximity(int x, int y)
+        {
+            int count = 0;
+            //mirar las  casillas a +1 y -1 de la casilla indicada para comprobar si hay bomba o no
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (IsBomb(IsOpen))
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        public bool IsBomb(Func<int, int, bool> isOpen)
         {
             throw new NotImplementedException();
         }
 
-        int IBoard.GetBombProximity(int x, int y)
+        public int GetHeight()
+        {
+            return _height;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public bool HasWin(int x, int y)
         {
             throw new NotImplementedException();
         }
 
-        int IBoard.GetHeight()
+        public void Init(int x, int y, int bombCount)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < _width; i++)
+            {
+                for (int j = 0; j < _height; j++)
+                {
+                    
+                }
+            }
+            
         }
 
-        int IBoard.GetWidth()
+        public bool IsBombAt(int x, int y)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i <= bombs.Length; i++)
+            {
+                if (bombs[i].position.x == x && bombs[i].position.y == y)
+                    return true;
+            }
+            return false;
         }
 
-        bool IBoard.HasWin(int x, int y)
+        public bool IsFlagAt(int x, int y)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i <= flags.Length; i++)
+            {
+                if (flags[i].position.x == x && flags[i].position.y == y)
+                    return true;
+            }
+            return false;
         }
 
-        void IBoard.Init(int x, int y, int bombCount)
+        public bool IsOpen(int x, int y, int IsOpen)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i <= cells.Length; i++)
+            {
+                if (cells[i].position.x == x && cells[i].position.y == y)
+                    return true;
+            }
+            return false;
         }
 
-        bool IBoard.IsBombAt(int x, int y)
+        public bool OpenCell(int x, int y)
         {
-            throw new NotImplementedException();
+            //abre una celda, puede ser un numero o una bomba
+            if (IsFlagAt(x, y) == false)
+                return true;
+            return false;
         }
 
-        bool IBoard.IsFlagAt(int x, int y)
+        public void PutFlagAt(int x, int y)
         {
-            throw new NotImplementedException();
-        }
-
-        bool IBoard.IsOpen(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
-
-        bool IBoard.OpenCell(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IBoard.PutFlagAt(int x, int y)
-        {
-            throw new NotImplementedException();
+            for (int i = 0; i < cells.Length; i++)
+                if (cells[i].position.x != x && cells[i].position.y != y)
+                {
+                    cells[i].SetFlag();
+                }
         }
     }
 }

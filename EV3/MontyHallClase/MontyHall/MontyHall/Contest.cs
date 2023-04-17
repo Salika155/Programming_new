8143a973546b145e9d6c2bfa1edcc5362477b465
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-04-18 00:18:25 +0200

    Add files via upload

---
+++ EV3/MontyHallClase/MontyHall/MontyHall/Contest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHall
+{
+    public enum DoorType
+    {
+        CAR, GOAT
+    }
+    public static class Contest
+    {
+        public void Init(int door);
+        
+        
+        public int ContestResult(bool wantsChange)
+        {
+            if ((wantsChange) == true)
+                return != ChooseDoor;
+                
+                   
+        }
+
+            //Concursante elige una puerta
+        public int ChooseDoor(List<Door> doors)
+        {
+            int count = 0;
+            for (int i = 1; i < doors.Count; i++);
+        }
+            //Presentador le ofrece cambiar de puerta
+
+        public int PresentadorDoorChosen(List<Door> doors)
+        {
+            if (doors.Count == 0)
+                ContestResult(false);
+
+        }
+            //Presentador abre una puerta que no es
+    }
+}
---
+++ EV3/MontyHallClase/MontyHall/MontyHall/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MontyHall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

32a71ffe69a6c2a5e0deee161de4a5124f947b1d
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-05-15 00:21:20 +0200

    Add files via upload

--- EV3/BaseDatos/BaseDatos/Program.cs
+++ EV3/BaseDatos/BaseDatos/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BaseDatos
 {
     public static class DataAcess
-     {
+    {
         private static SqlConnection SqlConnection;
         private static SqlCommand sqlCommand;
         private static SqlDataReader sqlDataReader;
@@ -15,9 +16,10 @@ namespace BaseDatos
             try
             {
                 using SqlConnection connection = new SqlConnection("Data Source=192.168.56.101,1433;Initial Catalog=PROGRAMACION;User Id=sa;Password=SqlServer123;Connection Timeout=30;");
-                {
-                    connection.Open();
-                    using (SqlCommand command = new SqlCommand("SELECT NAME_USER, SURNAME1 FROM USUARIO WHERE NAME_USER LIKE @filter", connection))
+                connection.Open();
+                
+                    
+                    using (SqlCommand command = new SqlCommand("SELECT NAME_USER, SURNAME1, AGE FROM USUARIO WHERE NAME_USER LIKE @filter", connection))
                     {
                         command.Parameters.AddWithValue("@filter", "%na%");
                         using (SqlDataReader reader = command.ExecuteReader())
@@ -27,16 +29,33 @@ namespace BaseDatos
                                 string NAME_USER = reader.GetFieldValue<string>(0);
                                 string SURNAME1 = reader.GetFieldValue<string>(1);
                                 int AGE = reader.GetFieldValue<int>(2);
-                                Console.WriteLine(reader["NAME_USER, SURNAME1, AGE"]);
-                                reader.Close();
-                                connection.Close();
+                                Console.WriteLine("NAME_USER, SURNAME1, AGE");
                             }
+                                reader.Close();
                         }
                     }
-                }
+
+                    using (SqlCommand command = new SqlCommand("INSERT INTO USUARIO (NAME_USER, SURNAME1) VALUES (@name, @surname)", connection))
+                    {
+                        command.Parameters.AddWithValue("@NAME", "Fernando");
+                        command.Parameters.AddWithValue("@SURNAME1", "Cortes");
+                        command.Parameters.AddWithValue("@AGE", "25");
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine($"Filas afectadas: {rowsAffected}");
+
+                    }
+
+                    using (SqlCommand command = new SqlCommand("EXEC sp_actualizar_edad_usuarios", connection))
+                    {
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine($"Filas afectadas: {rowsAffected}");
+                    }
+                    //hacer funcion
+                   
+                    using (SqlCommand command = new SqlCommand())
+                connection.Close();
             }
-            catch
-                (SqlException exception)
+            catch (SqlException exception)
                 when (exception.InnerException == null)
             {
                 Console.WriteLine("fallo de acceso en " + exception);

427a2cfa493a1c26334d0a1103396878edf39581
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-05-15 00:14:30 +0200

    Add files via upload

--- EV3/BuscaminasBiblio/BuscaminasApp/obj/Debug/net6.0/Buscaminas.AssemblyInfo.cs
+++ EV3/BuscaminasBiblio/BuscaminasApp/obj/Debug/net6.0/Buscaminas.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BuscaminasApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Buscaminas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("BuscaminasApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BuscaminasApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Buscaminas")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Buscaminas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.

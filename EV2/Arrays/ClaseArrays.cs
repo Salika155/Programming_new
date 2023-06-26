987f1916d303e183859c18a245596c8c67166826
Salika155 <carre155@hotmail.com>
2023-06-26 23:10:54 +0200

    i

---
+++ EV2/Arrays/ClaseArrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class ClaseArrays
+    {
+        public int IndexOf(int value, int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (value == array[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //for dentro de for, compara numero con valores siguientes, si es mayor lo cambia
+        public void Sort(int[] array) 
+        {
+            for (int i = 0; i < array.Length - 1; i++) 
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    array[i] = array[j];
+                    array[j] = array[i + 1];
+                }
+            }
+        }
+
+
+    }
+}
---
+++ EV2/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        Index Index = new Index();
+        public static void Main()
+        {
+
+            int[] array = { 1, 2, 3, 5, 8, 10 };
+            for (int i = 0; i < array.Length; i++) 
+            {
+                Console.WriteLine(array[i]);
+            }
+            
+        }
+    }
+}
+        

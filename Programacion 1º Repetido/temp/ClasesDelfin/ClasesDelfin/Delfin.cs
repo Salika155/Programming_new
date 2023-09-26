c7f71ab4b96edca9615c99878023ec320464cb44
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-09-26 19:48:18 +0200

    7
    746

--- Programacion 1º Repetido/temp/ClasesDelfin/ClasesDelfin/Delfin.cs
+++ Programacion 1º Repetido/temp/ClasesDelfin/ClasesDelfin/Delfin.cs
@@ -16,20 +16,49 @@ namespace ClasesDelfin
     public class Delfin
     {
         public double size;
-        public double life;
+        private double life;
+        public double lifecapacity;
         public string name;
         public ColorType color;
 
         //funcion de objeto
         public double GetLifePercent()
         {
-            return life / 10.0;
+            return life/10.0;
 
         }
 
         public bool IsAlive()
         {
             return life > 0.0;
+        }
+
+        public double GetLifePercentCapacity()
+        {
+            //return (life / lifecapacity) * 100;
+            return (life / lifecapacity);
+        }
+
+        public void SetLife(double life)
+        {
+            //cada vez que ejecuta el this vale el valor del objeto, si el primero vale 100 la primera vez valdra 100 y la segunda si el segundo objeto vale 200 valdra ese valor.
+            //this.life = life;
+            if (life < 0.0)
+                this.life = 0.0;
+            else if (life > lifecapacity)
+                this.life = lifecapacity;
+            else
+                this.life = life;
+
+            //que reviente el programa
+            //if (life < 0.0 || life > lifecapacity)
+            //throw new exception("Maaaaal")
+            //this.life = life;
+
+            //que no haga nada
+            //if (0.0 <= life && life <= lifecapacity)
+            //this.life = life;
+
         }
     }
 }

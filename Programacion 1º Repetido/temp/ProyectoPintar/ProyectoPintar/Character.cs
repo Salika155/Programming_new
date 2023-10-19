32d7b68dd6c6a974353989108a43908df6d6216d
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-10-19 18:15:40 +0200

    gred
    ger

---
+++ Programacion 1º Repetido/temp/ProyectoPintar/ProyectoPintar/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UDK;
+
+namespace ProyectoPintar
+{
+    public class Character : World
+    {
+        public double x;
+        public double y;
+        public double red, green, blue, alpha;
+
+        public Character(double x, double y, double red, double green, double blue, double alpha)
+        {
+            this.x = x;
+            this.y = y;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+
+        }
+
+        public void Draw(ICanvas canvas)
+        {
+            canvas.FillShader.SetColor(red, green, blue, alpha);
+            canvas.DrawRectangle(x, y, 1.5, 1.5);
+        }
+
+    }
+}
---
+++ Programacion 1º Repetido/temp/ProyectoPintar/ProyectoPintar/World.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPintar
+{
+    public class World : MyGame
+    {
+
+    }
+}

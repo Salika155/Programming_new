16205f420f645ca3559c210543f7f83aa3354c19
Salika155 <62901117+Salika155@users.noreply.github.com>
2023-10-24 19:46:35 +0200

    tg4ret
    rsdgds

---
+++ PROG/EV1/CoffeeMachine/CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+
+    public enum CoffeeMachineState
+        {
+        Esperando,
+        Procesando_Moneda,
+        Retirando_Producto,
+        Devolviendo_Cambio,
+        }
+
+    class CoffeeMachine
+    {
+        private CoffeeMachineState _state;
+
+        public CoffeeMachine()
+        {
+            _state = CoffeeMachineState.Esperando;
+        }
+
+        public CoffeeMachineState GetState()
+        {
+            return _state;
+        }
+
+        public void ChangeToNextState()
+        {
+            if (_state == CoffeeMachineState.Esperando)
+                _state = CoffeeMachineState.Procesando_Moneda;
+            else if (_state == CoffeeMachineState.Procesando_Moneda)
+                _state = CoffeeMachineState.Retirando_Producto;
+            else if (_state == CoffeeMachineState.Retirando_Producto)
+                _state = CoffeeMachineState.Devolviendo_Cambio;
+            else 
+                    _state = CoffeeMachineState.Esperando;
+        }
+    }
+}
---
+++ PROG/EV1/CoinChange/CoinChange/CoinChange.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinChange
+{
+
+    public enum CoinValor
+    {
+        C500,
+        C200,
+        C100,
+        C50,
+        C20,
+        C10,
+        C5,
+        C2,
+        C1,
+        C05,
+        C02,
+        C01,
+        C005,
+        C002,
+        C001
+    }
+
+    //resultado 34.15
+    //[20, 10, 2, 2, 0.1, 0.5]
+    public class CoinChange
+    {
+
+        private CoinValor coinValor;
+
+
+        //+ToNumber(coin:moneda):int
+        public static int ToNumber(CoinValor coinValor) //son en centimos
+        {
+            if (coinValor == CoinValor.C500)
+                return 50000;
+            if (coinValor == CoinValor.C200)
+                return 20000;
+            if (coinValor == CoinValor.C100)
+                return 10000;
+            if (coinValor == CoinValor.C50)
+                return 5000;
+            if (coinValor == CoinValor.C20)
+                return 2000;
+            if (coinValor == CoinValor.C10)
+                return 1000;
+            if (coinValor == CoinValor.C5)
+                return 500;
+            if (coinValor == CoinValor.C2)
+                return 200;
+            if (coinValor == CoinValor.C1)
+                return 100;
+            if (coinValor == CoinValor.C05)
+                return 50;
+            if (coinValor == CoinValor.C02)
+                return 20;
+            if (coinValor == CoinValor.C01)
+                return 10;
+            if (coinValor == CoinValor.C005)
+                return 5;
+            if (coinValor == CoinValor.C002)
+                return 2;
+            if (coinValor == CoinValor.C001)
+                return 1;
+            return 0;
+
+
+
+        }
+
+        //+ToMoneda(centims:int):Moneda
+        public static int ToMoneda()
+        {
+
+        }
+
+        //+GetCoins(centims:int):List<Moneda>
+        public CoinValor GetCoin()
+
+
+    }
+}

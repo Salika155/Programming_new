8bad7ba9d2a762117787be47c928c21ca4aeea52
Salika155 <carre155@hotmail.com>
2023-10-28 15:13:47 +0200

    kuik
    kukgh

---
+++ PROG/EV1/DominoPieces/DominoPieces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DominoPieces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

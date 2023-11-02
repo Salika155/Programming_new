14866d5c8be5b79b76b229f1b6f89125522378a3
Salika155 <carre155@hotmail.com>
2023-11-03 00:39:07 +0100

    dfcs
    sd

---
+++ PROG/EV1/Classes/Classes/ChessFigure.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public enum FigureColor
+    {
+        BLACK,
+        WHITE
+    }
+
+    public enum ChessFigureType
+    {
+        KNIGHT,
+        BISHOP,
+        PAWN,
+        QUEEN,
+        KING,
+        TOWER
+    }
+       
+
+    public class ChessFigure
+    {
+        private int _x;
+        private int _y;
+        private FigureColor _ColorType;
+        private ChessFigureType _FigureType;
+
+
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        public int GetX()
+        { 
+            return _x; 
+        }
+        public int GetY()
+        {
+            return _y;
+        }
+        public FigureColor GetColorType() 
+        {
+            return _ColorType;
+        }
+
+        public ChessFigureType GetFigureType() 
+        {
+            return _FigureType;
+        }
+
+        internal void MoveTo(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+    }
+}
---
+++ PROG/EV1/Classes/Classes/ChessUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class ChessUtils
+    {
+        List<ChessFigure> figures = new List<ChessFigure>();
+
+
+        public bool CanKnightMoveTo(ChessFigureType figureType, int targetX, int targetY, 
+            List<ChessFigure> allFigures)
+        {
+            return true;
+        }
+    }
+}

bb864999bbd94e280ce02d6d7e049a80cc2c9cc2
Salika155 <carre155@hotmail.com>
2023-11-03 12:49:42 +0100

    nbjbj

--- PROG/EV1/Classes/Classes/ChessFigure.cs
+++ PROG/EV1/Classes/Classes/ChessFigure.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public enum FigureColor
+    public enum ChessFigureColor
     {
         BLACK,
         WHITE
@@ -22,19 +22,33 @@ namespace Classes
         TOWER
     }
        
-
     public class ChessFigure
     {
         private int _x;
         private int _y;
-        private FigureColor _ColorType;
+        private ChessFigureColor _ColorType;
         private ChessFigureType _FigureType;
+        private int _movementCount; 
 
-
-        public bool IsValid()
+        public ChessFigure(int x, int y, ChessFigureColor colorType, ChessFigureType figureType)
         {
-            return true;
+            _x = x;
+            _y = y;
+            _ColorType = colorType;
+            _FigureType = figureType;
+            _movementCount = 0;
         }
+
+        //public bool IsValid()
+        //{
+        //    if (_FigureType == ChessFigureType.KING || _FigureType == ChessFigureType.QUEEN || 
+        //        _FigureType == ChessFigureType.KNIGHT || _FigureType == ChessFigureType.PAWN || 
+        //        _FigureType == ChessFigureType.TOWER || _FigureType == ChessFigureType.BISHOP) 
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
 
         public int GetX()
         { 
@@ -44,7 +58,7 @@ namespace Classes
         {
             return _y;
         }
-        public FigureColor GetColorType() 
+        public ChessFigureColor GetColorType() 
         {
             return _ColorType;
         }
@@ -56,8 +70,33 @@ namespace Classes
 
         internal void MoveTo(int x, int y)
         {
-            _x = x;
-            _y = y;
+            if ((x >= 0 && x <= 7) && (y >= 0 && y <= 7))
+            {
+                _x = x;
+                _y = y;
+                _movementCount++;
+            }
         }
+
+        public int GetMovementCount()
+        {
+            return _movementCount;
+        }
+
+        public bool HasBeenMoved()
+        {
+            return _movementCount > 0;
+        }
+
+        //public void ChessFigure promove()
+        //{
+
+        //}
+
+        //public void GetRectangle()
+        //{
+            
+        //}
+
     }
 }
--- PROG/EV1/Classes/Classes/ChessUtils.cs
+++ PROG/EV1/Classes/Classes/ChessUtils.cs
@@ -8,13 +8,33 @@ namespace Classes
 {
     public class ChessUtils
     {
-        List<ChessFigure> figures = new List<ChessFigure>();
+        private List<ChessFigure> figures = new List<ChessFigure>();
 
 
-        public bool CanKnightMoveTo(ChessFigureType figureType, int targetX, int targetY, 
-            List<ChessFigure> allFigures)
+        public bool CanKnightMoveTo(ChessFigureType figureType, int targetX, int targetY)
         {
+            
+
+
             return true;
+        }
+
+        public ChessFigure? GetFigureAt(int x, int y)
+        {
+            for (int i = 0; i > figures.Count; i++)
+            {
+                ChessFigure figure = figures[i];
+                if (figure.GetX() == x && figure.GetY() == y)
+                    return figure;
+            }
+            return null;
+        }
+
+        public bool IsOnTheTable(int x, int y)
+        {
+            ChessFigure? figure = GetFigureAt(x, y);
+            return figure != null;
+
         }
     }
 }

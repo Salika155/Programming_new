25ca7a19507a7fe0e9ef683a41d6068d1276a989
Salika155 <carre155@hotmail.com>
2023-12-02 15:59:27 +0100

    kjn
    hiuh

---
+++ otros/Examen1raEv2023-24/Examen1raEv2023-24/Classroom.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1raEv2023_24
+{
+    public class Classroom
+    {
+        private List<Student> _students = new List<Student>();
+        private string _nameClassroom;
+
+        public Classroom()
+        {
+
+        }
+
+        public Classroom(List<Student> students, string nameClassroom)
+        {
+            _students = students;
+            _nameClassroom = nameClassroom;
+        }
+
+        public string GetNameClassroom()
+        {
+            return _nameClassroom;
+        }
+
+        public void SetNameClassroom(string name)
+        {
+            _nameClassroom = name;
+        }
+
+        public void AddStuden(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void RemoveStudentAt(int index)
+        {
+            _students.RemoveAt(index);
+        }
+
+        public bool ContainStudentWithName(string name)
+        {
+            if (_students == null)
+                return false;
+
+            for (int i = 0; i < _students.Count; i++)
+            {
+                if (_students[i].GetName() != name)
+                    return false;
+            }
+            return true;
+
+        }
+
+        public void RemoveStudentsWithName(string name)
+        {
+            if (_students == null)
+                return;
+
+            for (int i = 0; i < _students.Count; i++)
+            {
+                if (_students[i].GetName() == name)
+                    _students.RemoveAt(i);
+            }
+        }
+
+        public Student? CreateStudent()
+        {
+            if (_students == null)
+                return null;
+
+            int count = _students.Count;
+            for (int i = 0; i < _students.Count; i++)
+            {
+                Student student = new Student();
+                _students.Add(student);
+
+            }
+            return _students[count];
+            
+        }
+    }
+}
---
+++ otros/Examen1raEv2023-24/Examen1raEv2023-24/Statistic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1raEv2023_24
+{
+    public class Statistic
+    {
+        private List<Student> students = new List<Student>();
+        public double GetAverageIMC(Classroom classroom)
+        {
+            return 0;
+        }
+
+        public Student? GetYoungestStudent(Classroom classroom)
+        {
+            if (students == null)
+                return null;
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                int min = int.MaxValue;
+                if (students[i].GetAge() >= min)
+                    min = students[i].GetAge();
+                i++;
+            }
+            return null;
+            
+        }
+
+        public Student? GetSortedStudentForSignature(Classroom classroom, Asignaturas asignaturas)
+        {
+            if (classroom == null || students == null)
+                return null;
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                //if (students.GetIMC())
+            }
+            return null;
+            
+
+        }
+
+        public GenderType GetStudentsWithGender()
+        {
+            
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].GetGenderType() == GenderType.M )
+                    return GenderType.M;
+                if (students[i].GetGenderType() == GenderType.F)
+                    return GenderType.F;
+            }
+            return GenderType.DESC;
+
+
+        }
+
+        public List<Student>? GetStadistics(Classroom classroom)
+        {
+            if (students == null || classroom == null)
+                return null;
+
+            return students;
+        }
+    }
+}

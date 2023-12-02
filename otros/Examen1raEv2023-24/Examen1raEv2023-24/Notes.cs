bbe51c72a619db654c3bb9f45062a928a0183704
Salika155 <carre155@hotmail.com>
2023-12-02 16:05:25 +0100

    4wtgret
    grgwf

---
+++ otros/Examen1raEv2023-24/Examen1raEv2023-24/Notes.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Examen1raEv2023_24
+{
+
+    public enum Asignaturas
+    {
+        MATEMATICAS,
+        LENGUAJE,
+        HISTORIA,
+        CIENCIA
+    }
+    
+    public class Notes
+    {
+        List<Student> students = new List<Student>();
+
+        private Asignaturas _asignaturas;
+        private double _nota;
+        
+
+        public Notes()
+        {
+
+        }
+
+        public Asignaturas GetAsignaturas()
+        {
+            if (_asignaturas == Asignaturas.MATEMATICAS)
+                return Asignaturas.MATEMATICAS;
+            if (_asignaturas == Asignaturas.LENGUAJE)
+                return Asignaturas.LENGUAJE;
+            if (_asignaturas == Asignaturas.CIENCIA)
+                return Asignaturas.CIENCIA;
+            else return Asignaturas.HISTORIA;
+        }
+
+        public double GetNota()
+        {
+            return _nota;
+        }
+
+        //no la completo
+        public int GetNotaWithName(string name, double? nota)
+        {
+            if (name == null || nota == null)
+                return -1;
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].GetName() == name && students[i].GetNota() == nota)
+                {
+                    List<Student> studentswithnota = new List<Student>();
+                    studentswithnota.Add(students[i]);
+                }
+                return i;
+            }
+            return 0;
+            
+        }
+
+        public void SetAsignatura(Asignaturas asignatura)
+        {
+            _asignaturas = asignatura;
+        }
+
+        public int GetQualificationForSignature(List<Student> student, Asignaturas asignaturas)
+        {
+            //for (int i = 0; i < students.Count; i++)
+            //{
+            //    for (int j = 0; j < _asignaturas.Count; j++)
+            //    {
+            //        if (student[i].GetNota() == _asignaturas[j].GetAsignature()
+            //    }
+
+            //}
+            return 0;
+        }
+
+
+        public void SetQualificationForSignature(Asignaturas asignaturas)
+        {
+
+
+        }
+
+        public double GetMediaAllAsignatures(Asignaturas asignaturas)
+        {
+            double media = 0;
+            for (int i = 0; i < students.Count; i++)
+            {
+
+
+            }
+            return media;
+        }
+
+        public Student? GetHigherAssignatureNote(Asignaturas asignaturas, Notes notas)
+        {
+            if (notas == null || students == null)
+                return null;
+
+            double max = double.MinValue;
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].GetNota() <= max && (asignaturas == Asignaturas.MATEMATICAS ||
+                    asignaturas == Asignaturas.CIENCIA || asignaturas == Asignaturas.LENGUAJE ||
+                    asignaturas == Asignaturas.HISTORIA))
+                {
+                    for(int j = 0; j < students.Count; j++)
+                    {
+                        if (max <= students[i].GetNota())
+                        {
+                            students[i].SetNota(max);
+                            i++;
+
+
+                            
+                        }
+                    }
+                    return students[i];
+
+                }
+            }
+            return null;
+        }
+
+        public Asignaturas GetLowerAssignatureNote()
+        {
+            return Asignaturas.MATEMATICAS;
+        }
+
+        public Asignaturas GetLowerCalification()
+        {
+            return Asignaturas.MATEMATICAS;
+        }
+
+        public List<Student>? GetHigherCalification()
+        {
+            if (students == null)
+                return null;
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                double max = double.MinValue;
+                if (students[i].GetNota() >= max)
+                {
+                    max = students[i].GetNota();
+                    i++;
+                }
+            }
+            return students;
+        }
+
+
+
+    }
+}
---
+++ otros/Examen1raEv2023-24/Examen1raEv2023-24/Student.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1raEv2023_24
+{
+    public enum GenderType
+    {
+        M,
+        F,
+        DESC
+    }
+    public class Student
+    {
+        private string _name;
+        private int _age;
+        private GenderType _gender;
+        private double _altura;
+        private double _peso;
+        private double _nota;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public Student(string name, int age, GenderType gender, double altura, double peso, double nota) : this(name, age)
+        {
+            _gender = gender;
+            _altura = altura;
+            _peso = peso;
+            _nota = nota;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        public GenderType GetGenderType()
+        {
+            if (_gender == GenderType.M)
+                return GenderType.M;
+            else return GenderType.F;
+        }
+
+        public double GetAltura()
+        {
+            return _altura;
+        }
+
+        public double GetPeso()
+        {
+            return _peso;
+        }
+
+        public double GetNota()
+        {
+            return _nota;
+        }
+
+        public void SetNota(double note)
+        {
+            _nota = note;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public void SetGenderType(GenderType gender)
+        {
+            _gender = gender;
+        }
+
+        public void SetAltura(int altura)
+        {
+            _altura = altura;
+        }
+
+        public void SetPeso(double peso)
+        {
+            _peso = peso;
+        }
+
+        public double GetIMC(double peso, double altura)
+        {
+            double IMC = peso / (altura * altura);
+            return IMC;
+        }
+
+        public bool IsElder()
+        {
+            if (_age < 18) return false; return true;
+        }
+
+        //public Student GetAverage(double nota)
+        //{
+
+        //}
+
+        public Student CloneStudent()
+        {
+            return new Student(_name, _age, _gender, _altura, _peso, _nota);
+            //nota no debe de cambiar si cambio la nota de un estudiante clonado, osea
+            //del que procede
+        }
+    }
+}

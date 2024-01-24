f4ef107134ecf8654520b0f4a4d0fe8d8d380a0b
Salika155 <carre155@hotmail.com>
2024-01-24 14:15:15 +0100

    jb
    gogi

---
+++ PROG/EV2/RESIDUAL/DiccionarioPruebaBasura/DiccionarioPruebaBasura/DiccionarioSimple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiccionarioPruebaBasura
+{
+    public class DiccionarioSimple<K, V>
+    {
+
+        //Ejercicios sobre Diccionarios:
+        //1. Diccionario Simple:
+        //Crea una clase llamada MyDictionary<K, V> que implemente un diccionario simple.
+        //Debe tener métodos para agregar, eliminar y obtener elementos, y para verificar si una clave existe.
+
+        private DiccionarioSimple<K, V> _diccionario = new DiccionarioSimple<K, V>();
+
+        public void Add(K key, V value)
+        {
+            _diccionario.Add(key, value);
+        }
+
+        //remove
+        //getvalue
+        //contains
+
+
+
+    }
+}
---
+++ PROG/EV2/RESIDUAL/DiccionarioPruebaBasura/DiccionarioPruebaBasura/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiccionarioPruebaBasura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

d27e0ea8bd522c7c29e61449ff12ee5dc50411aa
Salika155 <carre155@hotmail.com>
2024-01-28 17:32:23 +0100

    yiy
    yi

---
+++ PROG/EV2/EVALUABLE/DamLibTest/DamLib/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamLib
+{
+    public class Tree
+    {
+        private class Node<T>
+        {
+            T Content;
+            List<Node<T>> _children = new List<Node<T>>();
+            Node<T> _parent;
+
+            public Node(T content)
+            {
+                Content = content;
+            }
+        }
+    }
+}

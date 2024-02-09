e64c4ee25d8caf778a11528a6fdec7bf1c2d12b2
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-02-09 18:47:37 +0100

    sdad
    sadc

---
+++ PROG/EV2/EVALUABLE/TPV/TPVLib/ITPV.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace TPVLib
+{
+    public interface ITPV
+    {
+        static ITPV CreateNewTPV()
+        {
+            return new RAMTPV();
+        }
+
+        //hay que hacer el add product
+    }
+}

2b6768b933ddcaad0ee0dd1e3c6071329e4750bf
Salika155 <carre155@hotmail.com>
2024-02-19 12:08:46 +0100

    jer
    rjr

---
+++ PROG/EV2/EVALUABLE/TPV/TPVLib/RAMDatabase.cs
@@ -0,0 +1,48 @@
+﻿
+namespace TPVLib
+{
+    internal class RAMDatabase : IDatabase
+    {
+        public int ProductCount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public long AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTicketHeader(TicketHeader header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTicketLine(TicketLine line)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product? GetProduct(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts(int offset, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProduct(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProductWithId(long id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDatabase.AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

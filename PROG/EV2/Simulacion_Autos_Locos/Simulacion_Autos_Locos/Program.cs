adff7cb00c95face784ebcb99872e8c0460c5a55
Salika155 <carre155@hotmail.com>
2024-03-16 13:16:35 +0100

    autos_locos
    simulacro

---
+++ PROG/EV2/Simulacion_Autos_Locos/Simulacion_Autos_Locos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulacion_Autos_Locos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

cd70a8736212bc579e1b0fc3dbe4a9c5b22a4591
Salika155 <carre155@hotmail.com>
2024-03-23 12:52:09 +0100

    kl
    kl

---
+++ PROG/EV3/Proyecto_Detecta_Copias/Proyecto_Detecta_Copias/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_Detecta_Copias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

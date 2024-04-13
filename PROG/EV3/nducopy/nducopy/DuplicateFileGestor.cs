d1480d8c4171e50b80d2c979a66035248e890159
Salika155 <carre155@hotmail.com>
2024-04-13 20:10:49 +0200

    jf
    fhf

--- PROG/EV3/nducopy/nducopy/DuplicateFileGestor.cs
+++ PROG/EV3/nducopy/nducopy/DuplicateFileGestor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,14 +23,81 @@ namespace nducopy
         //NOMBRE DE FICHERO ALGO QUE INDIQUE LA FECHA DE CREACION DEL FICHERO.
         //FUNCION PARA COMPARAR DOS FICHEROS QUE NO NECESITEN QUE EL CONTENIDO ENTERO DEL FICHERO
         //ESTE EN MEMORIA RAM.
-        private string _pathOut = "";
+        private string _pathOut = string.Empty;
         private List<string> _directories = new List<string>();
         private List<FileUser> _files = new List<FileUser>();
         private List<FileUser> _duplicates = new List<FileUser>();
         private bool _isCopy = false;
         private bool _isSuccess = false;
 
+        public bool IsSetPathOut => !string.IsNullOrEmpty(_pathOut);
         public bool HasDirectories => _directories.Count > 0;
         public bool HasFiles => _files.Count > 0;
+        public bool ExportDuplicates { get; set; }
+        public bool ControlOutputContain { get; set; }
+
+        public void AddDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                _directories.Add(path);
+            }
+        }
+
+        public void SetPathOut(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string newPath = Path.Combine(path, "newFileCombined");
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                _pathOut = newPath;
+            }
+        }
+
+        public void ListFiles()
+        {
+            foreach (string directory in _directories)
+            {
+                string[] allfiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+                foreach (string file in allfiles)
+                {
+                    FileUser fileUser = new FileUser(file)
+                    {
+                        Path = file,
+                        Hash = FileGestor.GetHashFromFile(file),
+                        Size = new FileInfo(file).Length,
+                    };
+                    _files.Add(fileUser);
+                }
+            }
+        }
+
+        public void CompareFiles()
+        {
+            for (int i = 0; i < _files.Count; i++)
+            {
+                if (_files[i].Disabled)
+                    continue;
+                for (int j = i + 1; j < _files.Count; j++)
+                {
+                    var file1 = _files[i];
+                    var file2 = _files[j];
+                    if (file2.Disabled)
+                        continue;
+                    if (file1.Hash == file2.Hash && file1.Size == file2.Size)
+                    {
+                        file2.Disabled = true;
+                        _duplicates.Add(file2);
+
+                    }
+                }
+            }
+        }
+
+
+
     }
 }

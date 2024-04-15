88e315def817fd08af22eba87c9406ee368080cd
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-04-15 16:54:34 +0200

    fefef
    fesfcs

---
+++ PROG/EV3/MiPrimerWPF/MiPrimerWPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+
+namespace MiPrimerWPF
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Botón apretado");
+            bool? state = CheckboxMain.IsChecked.Value;
+            CheckboxMain.IsChecked = !state;
+
+            this.ButtonMain.Content += "0";
+        }
+
+        private void OnMainClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void OnCheck(object sender, RoutedEventArgs e)
+        //{
+        //    bool? state = CheckboxMain.IsChecked.Value;
+        //    CheckboxMain.IsChecked = !state; 
+        //}
+    }
+}

8df67afebe6cd618179162ea079e50d13a511d31
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-04-17 15:13:31 +0200

    dwe
    wedfc

--- EV3/BuscaminasBiblio/BuscaminasApp/obj/Debug/net6.0/Buscaminas.AssemblyInfo.cs
+++ EV3/BuscaminasBiblio/BuscaminasApp/obj/Debug/net6.0/Buscaminas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Buscaminas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c91ee47db9658cf6c433c4906ede61cdfe51dc77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Buscaminas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Buscaminas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

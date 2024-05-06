f75b2dde63262e3faf3907ab070b8b59d086d782
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-05-06 16:54:39 +0200

    e32w
    w3er

---
+++ PROG/EV3/TestDB/TestDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+;
+
+namespace TestDB
+{
+    internal class Program
+    {
+        public const string ConnectionString = "Data Source=192.168.56.101; Initial Catalog=WPF_APP_BD; ID=sa; Password=SqlServer123";
+        public static void TestConnection()
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            connection.Dispose();
+
+        }
+        static void Main(string[] args)
+        {
+            TestConnection();
+        }
+    }
+}

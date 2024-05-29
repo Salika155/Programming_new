6f8614288d03f8c30e0a63991801d4c8c9167221
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-05-29 15:16:21 +0200

    cvx
    v xvvxd

--- PROG/EV3/WPF_BacklogSolution/WPF_BacklogData/obj/Debug/net7.0/WPF_BacklogData.AssemblyInfo.cs
+++ PROG/EV3/WPF_BacklogSolution/WPF_BacklogData/obj/Debug/net7.0/WPF_BacklogData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd4757b9b0cd682994734f07e715f48aff7ed47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

95647e9febfb99d1fa01be2decf54de81f2fab3d
Salika155 <carre155@hotmail.com>
2024-05-31 13:48:51 +0200

    fumg
    kmguk

--- PROG/EV3/WPF_BacklogSolution/WPF_BacklogApp/obj/Debug/net7.0-windows/WPF_BacklogApp.AssemblyInfo.cs
+++ PROG/EV3/WPF_BacklogSolution/WPF_BacklogApp/obj/Debug/net7.0-windows/WPF_BacklogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_BacklogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d164df3270a3647ace6bdc84e01136a53f398f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_BacklogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_BacklogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

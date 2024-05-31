209a7164442eab019da64c8afbc204deea5e60f1
Salika155 <carre155@hotmail.com>
2024-05-31 02:37:30 +0200

    i7tuik
    gyku

---
+++ PROG/EV3/WPF_BacklogSolution/WPF_BacklogApp/GameDetailsWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_BacklogData.Models;
+
+namespace WPF_BacklogApp
+{
+    /// <summary>
+    /// Lógica de interacción para GameDetailsWindow.xaml
+    /// </summary>
+    public partial class GameDetailsWindow : Window
+    {
+        public Game Game { get; set; }
+        public GameDetailsWindow(Game game)
+        {
+            InitializeComponent();
+            Game = game;
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- PROG/EV3/WPF_BacklogSolution/WPF_BacklogData/obj/Debug/net7.0/WPF_BacklogData.AssemblyInfo.cs
+++ PROG/EV3/WPF_BacklogSolution/WPF_BacklogData/obj/Debug/net7.0/WPF_BacklogData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6371ea1cf053c85db3119279f74ae0ce159cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d164df3270a3647ace6bdc84e01136a53f398f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_BacklogData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

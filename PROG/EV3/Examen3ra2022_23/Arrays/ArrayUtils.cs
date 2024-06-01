7e082d479e92bf10fbce278eb1958e810b9aaddc
Salika155 <carre155@hotmail.com>
2024-06-01 12:41:43 +0200

    utf6
    tu

---
+++ PROG/EV3/Examen3ra2022_23/Arrays/ArrayUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class ArrayUtils
+    {
+
+        public static int FindMaxValorInArray(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return -1;
+
+            int max = 0;
+
+            foreach (var m in array)
+            {
+                if (m > max)
+                    max = m;
+            }
+            return max;
+        }
+
+        public static double[] RemoveAllOccurrences(double[] array, double value)
+        {
+
+        }
+    }
+}
---
+++ PROG/EV3/Examen3ra2022_23/Examen3ra2022_23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen3ra2022_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

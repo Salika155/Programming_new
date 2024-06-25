b4a1301dff311d535e77dbc48d948fca6d988d39
Salika155 <carre155@hotmail.com>
2024-06-25 12:33:42 +0200

    kj
    j

--- PROG/EV2/Examen/ExamenLogica2/ExamenLogica2/F24.cs
+++ PROG/EV2/Examen/ExamenLogica2/ExamenLogica2/F24.cs
@@ -4,41 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExamenLogica2
-{
-    internal class F24
-    {
-        public class A
-        {
-            public int n1;
-            protected int n2;
-        }
+//namespace ExamenLogica2
+//{
+//    internal class F24
+//    {
+//        public class A
+//        {
+//            public int n1;
+//            protected int n2;
+//        }
 
-        public class B : A
-        {
-            public B()
-            {
-                n2 = 1;
-            }
-        }
+//        public class B : A
+//        {
+//            public B()
+//            {
+//                n2 = 1;
+//            }
+//        }
 
-        public class C : B
-        {
-            public C() { n1 = 2; }
+//        public class C : B
+//        {
+//            public C() { n1 = 2; }
 
-            public int GetN()
-            {
-                return n2 - n1;
-            }
-        }
+//            public int GetN()
+//            {
+//                return n2 - n1;
+//            }
+//        }
 
-        public static void Main()
-        {
-            C aux1 = new C();
-            Console.Write(aux1.GetN());
-        }
-    }
-    //Respuesta: n1 vale 2 y n2 vale 1, al pasarselo por b y  depender de a
-    //hace la operacion de getn.
-    //Solucion -1
-}
+//        public static void Main()
+//        {
+//            C aux1 = new C();
+//            Console.Write(aux1.GetN());
+//        }
+//    }
+//    //Respuesta: n1 vale 2 y n2 vale 1, al pasarselo por b y  depender de a
+//    //hace la operacion de getn.
+//    //Solucion -1
+//}
